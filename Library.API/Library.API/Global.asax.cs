9fef83782c40577bd75fb813f644be7a52a037a6
DESKTOP-8OBM2CC\Kristina <learningwithcrispyna@gmail.com>
2018-05-03 20:02:22 +0300

    Added another name to solution

--- Library.API/Library.API/Global.asax.cs
+++ Library.API/Library.API/Global.asax.cs
@@ -17,7 +17,7 @@ namespace Library.API
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
         }
     }
 }
